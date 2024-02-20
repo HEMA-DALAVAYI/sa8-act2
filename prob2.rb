671ff7114b8284e232c78617781d35c94cd9ee5f
Hema Dalavayi <gayathri.dhs@gmail.com>
2024-02-19 22:03:42 -0600

    Used the attr_reader and attr_writer for the Gadget class

---
+++ prob2.rb
@@ -0,0 +1,23 @@
+'''
+Problem 2: Using attr_reader and attr_writer
+Objective: Create a Gadget class with name and price attributes.
+Use attr_reader for name and attr_writer for price.
+Demonstrate how to read the name and update the price of a Gadget instance.
+'''
+
+class Gadget
+  def initialize(name,price)
+    @name = name
+    @price = price
+  end
+  attr_reader  :name , :price
+  attr_writer  :price
+end
+
+gadget1 = Gadget.new('Iphone-14', 1000)
+puts gadget1.name
+puts "Before Change"
+puts gadget1.price
+puts "After Change"
+gadget1.price = 500
+puts gadget1.price
